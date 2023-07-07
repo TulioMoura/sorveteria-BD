543c1d5b55e6cbcdfefafb329cd8dc6886482f70
Raul772 <raulvictor772@gmail.com>
2023-07-07 19:27:12 -0300

    Add Custom Danger Button CSS

--- frontend/src/components/global/TableItem.tsx
+++ frontend/src/components/global/TableItem.tsx
@@ -10,9 +10,9 @@ export default function TableItem(props: listItemProps) {
     <tr>
       <td className="font-quicksand py-2 m-1">{props.title}</td>
       <td className="font-quicksand py-2 m-1">{props.cpf}</td>
-      <td>
+      <td className="flex justify-end">
         {props.editar && <a className="customButton" href={props.editar}>Editar</a>}
-        {props.delete && <a className="customButton" href={props.delete}>Deletar</a>}
+        {props.delete && <a className="customDangerButton" href={props.delete}>Deletar</a>}
       </td>
     </tr>
   )
--- frontend/src/pages/Clientes.tsx
+++ frontend/src/pages/Clientes.tsx
@@ -18,18 +18,22 @@ export default function Clientes() {
   return (
     <section className="container-fluid header bg-rv-pale min-h-screen ">
       <h1 className="font-black font-DancingScript text-3xl text-center p-5">Clientes</h1>
-      <div className="content-container flex justify-center items-center">
-        <table className="w-full divide-y divide-stone-500 divide-dotted">
-          <tr>
-            <th className="font-quicksand text-left">Nome</th>
-            <th className="font-quicksand text-left">CPF</th>
-            <th className="font-quicksand text-left">Opções</th>
-          </tr>
-          <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
-          <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
-          <TableItem title="Teste com mais palavras" cpf="726351412" editar="#" delete="#" />
-          <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
-          <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
+      <div className="content-container flex justify-center items-center overflow-scroll">
+        <table className="w-full">
+          <thead className="border-b-2 border-stone-500">
+            <tr>
+              <th className="font-quicksand text-left">Nome</th>
+              <th className="font-quicksand text-left">CPF</th>
+              <th className="font-quicksand"></th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-stone-500 divide-dotted">
+            <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
+            <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
+            <TableItem title="Teste com mais palavras" cpf="726351412" editar="#" delete="#" />
+            <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
+            <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
+          </tbody>
         </table>
       </div>
     </section>
