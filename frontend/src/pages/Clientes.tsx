282ffd851f82372fc1f8590733ef152894486f93
Raul772 <raulvictor772@gmail.com>
2023-07-10 12:55:38 -0300

    install Axios

--- frontend/src/pages/Clientes.tsx
+++ frontend/src/pages/Clientes.tsx
@@ -18,7 +18,7 @@ export default function Clientes() {
   return (
     <section className="container-fluid header bg-rv-pale min-h-screen ">
       <h1 className="font-black font-DancingScript text-3xl text-center p-5">Clientes</h1>
-      <div className="content-container flex justify-center items-center overflow-scroll">
+      <div className="content-container flex justify-center items-center overflow-auto">
         <table className="w-full">
           <thead className="border-b-2 border-stone-500">
             <tr>
@@ -28,11 +28,11 @@ export default function Clientes() {
             </tr>
           </thead>
           <tbody className="divide-y divide-stone-500 divide-dotted">
-            <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
-            <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
-            <TableItem title="Teste com mais palavras" cpf="726351412" editar="#" delete="#" />
-            <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
-            <TableItem title="Teste" cpf="726351412" editar="#" delete="#" />
+            <TableItem title="Teste" cpf="72635154122" editar="#" delete="#" />
+            <TableItem title="Teste" cpf="72635145412" editar="#" delete="#" />
+            <TableItem title="Teste com mais palavras" cpf="72635641412" editar="#" delete="#" />
+            <TableItem title="Teste" cpf="72635155412" editar="#" delete="#" />
+            <TableItem title="Teste" cpf="72635812412" editar="#" delete="#" />
           </tbody>
         </table>
       </div>
