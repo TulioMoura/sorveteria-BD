2d12d62aae2cee61e43c489fb31093607f7f3f86
Túlio <90994955+TulioMoura@users.noreply.github.com>
2023-07-15 19:22:36 -0300

    Disabled Cors.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 const express = require("express")
 require("dotenv").config()
+const cors = require("cors")
 const router = require("./routes/routes.js")
 const errorHandlerMiddleware = require("./middlewares/errorHandlerMiddleware.js")
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use("/", router)
 
