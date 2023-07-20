4fc93fed38160e06fc044c286e20131e3951a640
Túlio <90994955+TulioMoura@users.noreply.github.com>
2023-07-20 14:10:08 -0300

    added stored procedure creation.

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -3,4 +3,15 @@ const path = require("path")
 const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING)
 sequelize.sync().then(console.log("Database checked! "))
 
+sequelize.query(`CREATE OR REPLACE PROCEDURE resetLucro() 
+
+as $$
+	BEGIN
+	update Produtos
+	set lucro = 0.15;
+END;
+$$ 
+language plpgsql;
+
+`).then(v=> console.log("Procedure criada"))
 module.exports = sequelize
